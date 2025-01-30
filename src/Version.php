b34591c390be7689ecccf42baf3c9a52de1d888c
Pete Bishop <peter.bishop@evie.software>
2025-01-30 15:51:54 +0000

    Reduce duplicated code

--- src/Version.php
+++ src/Version.php
@@ -2,10 +2,13 @@
 
 namespace Petebishwhip\NativePhpCli;
 
+use Petebishwhip\NativePhpCli\Traits\PackageVersionRetrieverTrait;
 use z4kn4fein\SemVer\Version as SemanticVersion;
 
 class Version
 {
+    use PackageVersionRetrieverTrait;
+
     public const VERSION = '1.0.0-beta.1';
     private const CACHE_FILE = __DIR__ . '/../cache/version_cache.json';
     private const CACHE_TTL = 3600; // 1 Hour
@@ -25,29 +28,8 @@ class Version
             return SemanticVersion::parseOrNull($cachedVersion);
         }
 
-        $ch = curl_init();
+        $latestVersion = self::getVersionForPackage('petebishwhip/nativephp-cli');
 
-        curl_setopt($ch, CURLOPT_URL, 'https://api.github.com/repos/petebishwhip/nativephp-cli/releases/latest');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, [
-            'User-Agent: NativeCLI/Updater'
-        ]);
-
-        $response = curl_exec($ch);
-
-        if ($response === false) {
-            throw new Exception('Failed to fetch latest version: ' . curl_error($ch));
-        }
-
-        curl_close($ch);
-
-        $data = json_decode($response, true);
-
-        if (json_last_error() !== JSON_ERROR_NONE) {
-            throw new Exception('Failed to decode latest version');
-        }
-
-        $latestVersion = $data['tag_name'];
         self::cacheVersion($latestVersion);
 
         return SemanticVersion::parseOrNull($latestVersion);
