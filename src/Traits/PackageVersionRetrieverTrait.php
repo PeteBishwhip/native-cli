a22a423ca1ed35afceb96c0c2b6da4f12ef7a434
Pete Bishop <peter.bishop@evie.software>
2025-01-30 16:42:52 +0000

    Cache all the versions for an hour

--- src/Traits/PackageVersionRetrieverTrait.php
+++ src/Traits/PackageVersionRetrieverTrait.php
@@ -2,6 +2,7 @@
 
 namespace Petebishwhip\NativePhpCli\Traits;
 
+use Illuminate\Support\Collection;
 use Petebishwhip\NativePhpCli\Exception;
 use Petebishwhip\NativePhpCli\Exception\RateLimitedException;
 use RuntimeException;
@@ -16,6 +17,10 @@ trait PackageVersionRetrieverTrait
      */
     public static function getVersionForPackage(string $package, string $tag = 'latest'): ?SemanticVersion
     {
+        if (self::checkCache($package) !== null) {
+            return SemanticVersion::parseOrNull(self::checkCache($package));
+        }
+
         $url = sprintf(
             'https://api.github.com/repos/%s/releases/latest',
             $package
@@ -52,6 +57,70 @@ trait PackageVersionRetrieverTrait
 
         $latestVersion = $data['tag_name'];
 
+        self::cacheVersion($package, $latestVersion);
+
         return SemanticVersion::parseOrNull($latestVersion);
+    }
+
+    private static function checkCache(string $package)
+    {
+        $cacheData = self::getCacheData();
+
+        $packageData = $cacheData->where('package', $package)->first();
+
+        if (empty($packageData)) {
+            return null;
+        }
+
+        if (time() - $packageData['timestamp'] > 3600) {
+            return null;
+        }
+
+        return $packageData['version'];
+    }
+
+    private static function getCacheFileLocation(): string
+    {
+        return __DIR__ . '/../../cache/version_cache.json';
+    }
+
+    private static function cacheVersion(string $package, string $version): void
+    {
+        if (!file_exists(dirname(self::getCacheFileLocation()))) {
+            mkdir(dirname(self::getCacheFileLocation()), 0777, true);
+        }
+
+        $cacheData = self::getCacheData();
+
+        $packageInfo = $cacheData->where('package', $package)->first();
+
+        if ($packageInfo !== null) {
+            $cacheData = $cacheData->reject(function ($item) use ($package) {
+                return $item['package'] === $package;
+            });
+        }
+
+        $cacheData->push([
+            'package' => $package,
+            'version' => $version,
+            'timestamp' => time(),
+        ]);
+
+        file_put_contents(self::getCacheFileLocation(), json_encode($cacheData->toArray()));
+    }
+
+    private static function getCacheData(): ?Collection
+    {
+        if (!file_exists(self::getCacheFileLocation())) {
+            return collect();
+        }
+
+        $info = json_decode(file_get_contents(self::getCacheFileLocation()), true);
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            return collect();
+        }
+
+        return collect($info);
     }
 }
--- src/Version.php
+++ src/Version.php
@@ -10,8 +10,6 @@ class Version
     use PackageVersionRetrieverTrait;
 
     public const VERSION = '1.0.0-beta.1';
-    private const CACHE_FILE = __DIR__ . '/../cache/version_cache.json';
-    private const CACHE_TTL = 3600; // 1 Hour
 
     public static function get(): ?SemanticVersion
     {
@@ -23,44 +21,8 @@ class Version
      */
     public static function getLatestVersion(): ?SemanticVersion
     {
-        $cachedVersion = self::getCachedVersion();
-        if ($cachedVersion !== null) {
-            return SemanticVersion::parseOrNull($cachedVersion);
-        }
-
-        $latestVersion = self::getVersionForPackage('petebishwhip/nativephp-cli');
-
-        self::cacheVersion($latestVersion);
-
-        return SemanticVersion::parseOrNull($latestVersion);
+        return self::getVersionForPackage('petebishwhip/nativephp-cli');
     }
 
-    private static function getCachedVersion(): ?string
-    {
-        if (!file_exists(self::CACHE_FILE)) {
-            return null;
-        }
 
-        $cacheData = json_decode(file_get_contents(self::CACHE_FILE), true);
-
-        if (json_last_error() !== JSON_ERROR_NONE || time() - $cacheData['timestamp'] > self::CACHE_TTL) {
-            return null;
-        }
-
-        return $cacheData['version'];
-    }
-
-    private static function cacheVersion(string $version): void
-    {
-        $cacheData = [
-            'version' => $version,
-            'timestamp' => time(),
-        ];
-
-        if (!file_exists(dirname(self::CACHE_FILE))) {
-            mkdir(dirname(self::CACHE_FILE), 0777, true);
-        }
-
-        file_put_contents(self::CACHE_FILE, json_encode($cacheData));
-    }
 }
