172f86c9f005fe289e1a197b2f99137fd7a19fc0
Pete Bishop <peter.bishop@evie.software>
2025-01-31 16:24:10 +0000

    Rectify invalid class usage

--- src/Command/ClearCacheCommand.php
+++ src/Command/ClearCacheCommand.php
@@ -2,10 +2,10 @@
 
 namespace Petebishwhip\NativePhpCli\Command;
 
-use Illuminate\Console\QuestionHelper;
 use Petebishwhip\NativePhpCli\Cache;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
